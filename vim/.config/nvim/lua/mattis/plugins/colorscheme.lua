cb76020cc58be3be3ff6e765eeea381bee7f5480
Mattis Megevand (mmeg) <mmeg@odoo.com>
2024-02-27 11:15:27 +0100

    fix bg

--- vim/.config/nvim/lua/mattis/plugins/colorscheme.lua
+++ vim/.config/nvim/lua/mattis/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
     "mcchrish/zenbones.nvim",
     config = function()
         vim.opt.termguicolors = true
-        vim.opt.background = light
+        vim.opt.background = "light"
         vim.g.bones_compat = true
         vim.cmd.colorscheme("zenbones")
     end,
