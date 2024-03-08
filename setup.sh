13857692936cc8ac27348d8376c23e6139e8dc35
Mattis Megevand <megevand.mattis@gmail.com>
2024-03-08 19:32:37 +0100

    update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ stow vim
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     stow mac
+    defaults write -g ApplePressAndHoldEnabled -bool false
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     stow linux 
 fi
