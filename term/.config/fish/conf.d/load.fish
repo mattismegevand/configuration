6355edddcde09afd310ee981a0957804e9fdaa5d
Mattis Megevand <megevand.mattis@gmail.com>
2024-12-22 17:06:15 +0100

    config cleanup

--- term/.config/fish/conf.d/load.fish
+++ term/.config/fish/conf.d/load.fish
@@ -9,14 +9,4 @@ if status is-interactive
     if test -f ~/.aliases_work
         source ~/.aliases_work
     end
-
-    if test -f ~/.cargo/env.fish
-        source "$HOME/.cargo/env.fish"
-    end
-
-    if string match -q "*macbook*" (hostname)
-        source ~/.asdf/asdf.fish
-    end
-
-    fzf --fish | source
 end
