8395426b68fca40b3092e4798304f3ed613646e8
Mattis Megevand <megevand.mattis@gmail.com>
2026-02-02 22:17:24 +0100

    .

--- nix/modules/home/darwin.nix
+++ nix/modules/home/darwin.nix
@@ -1,17 +1,18 @@
-{ username, ... }:
+{ config, username, ... }:
 
 let
   configDir = ../../..;
   fontDir = "/Users/${username}/Library/Mobile Documents/com~apple~CloudDocs/misc/250223LKQVJ0407L/TX-02-4K4X9N9Y";
+  mkSymlink = config.lib.file.mkOutOfStoreSymlink;
 in
 {
   imports = [ ./common.nix ];
 
   home.file = {
-    "Library/Fonts/BerkeleyMono-Regular.otf".source = "${fontDir}/BerkeleyMono-Regular.otf";
-    "Library/Fonts/BerkeleyMono-Bold.otf".source = "${fontDir}/BerkeleyMono-Bold.otf";
-    "Library/Fonts/BerkeleyMono-Oblique.otf".source = "${fontDir}/BerkeleyMono-Oblique.otf";
-    "Library/Fonts/BerkeleyMono-Bold-Oblique.otf".source = "${fontDir}/BerkeleyMono-Bold-Oblique.otf";
+    "Library/Fonts/BerkeleyMono-Regular.otf".source = mkSymlink "${fontDir}/BerkeleyMono-Regular.otf";
+    "Library/Fonts/BerkeleyMono-Bold.otf".source = mkSymlink "${fontDir}/BerkeleyMono-Bold.otf";
+    "Library/Fonts/BerkeleyMono-Oblique.otf".source = mkSymlink "${fontDir}/BerkeleyMono-Oblique.otf";
+    "Library/Fonts/BerkeleyMono-Bold-Oblique.otf".source = mkSymlink "${fontDir}/BerkeleyMono-Bold-Oblique.otf";
   };
 
   xdg.configFile = {
