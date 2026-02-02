cde7c19990fc6aea6fb8282be2754f904981f11f
Mattis Megevand <megevand.mattis@gmail.com>
2026-02-02 22:12:38 +0100

    .

--- nix/modules/home/common.nix
+++ nix/modules/home/common.nix
@@ -82,5 +82,13 @@ in
       enable = true;
       nix-direnv.enable = true;
     };
+    ssh = {
+      enable = true;
+      extraConfig = ''
+        Host *
+            IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+      includes = [ "~/.orbstack/ssh/config" ];
+    };
   };
 }
