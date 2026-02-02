e8189fa7aecf370075a1c1bba5bb3c877d33a4cf
Mattis Megevand <megevand.mattis@gmail.com>
2026-02-02 22:16:53 +0100

    .

--- nix/modules/home/common.nix
+++ nix/modules/home/common.nix
@@ -85,8 +85,8 @@ in
     ssh = {
       enable = true;
       enableDefaultConfig = false;
-      includes = [ "~/.orbstack/ssh/config" ];
-      matchBlocks."*" = {
+      includes = pkgs.lib.optionals pkgs.stdenv.isDarwin [ "~/.orbstack/ssh/config" ];
+      matchBlocks."*" = pkgs.lib.mkIf pkgs.stdenv.isDarwin {
         extraOptions = {
           IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
         };
--- nix/modules/home/darwin.nix
+++ nix/modules/home/darwin.nix
@@ -1,10 +1,18 @@
-{ ... }:
+{ username, ... }:
 
 let
   configDir = ../../..;
+  fontDir = "/Users/${username}/Library/Mobile Documents/com~apple~CloudDocs/misc/250223LKQVJ0407L/TX-02-4K4X9N9Y";
 in
 {
   imports = [ ./common.nix ];
+
+  home.file = {
+    "Library/Fonts/BerkeleyMono-Regular.otf".source = "${fontDir}/BerkeleyMono-Regular.otf";
+    "Library/Fonts/BerkeleyMono-Bold.otf".source = "${fontDir}/BerkeleyMono-Bold.otf";
+    "Library/Fonts/BerkeleyMono-Oblique.otf".source = "${fontDir}/BerkeleyMono-Oblique.otf";
+    "Library/Fonts/BerkeleyMono-Bold-Oblique.otf".source = "${fontDir}/BerkeleyMono-Bold-Oblique.otf";
+  };
 
   xdg.configFile = {
     "ghostty/config".source = "${configDir}/config/ghostty/config";
