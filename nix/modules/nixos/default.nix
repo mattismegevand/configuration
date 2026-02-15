4c4a4a347c0329e8749ea9cef9a34fe98ad3a41d
Mattis Megevand <megevand.mattis@gmail.com>
2026-02-15 23:04:46 +0100

    .

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -16,6 +16,10 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "root" username ];
+      extra-substituters = [ "https://cache.garnix.io" ];
+      extra-trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
     gc = {
       automatic = true;
