a9eea2c930ca885900fd70d0e7594e41ce575bf8
Mattis Megevand <megevand.mattis@gmail.com>
2026-06-25 19:24:33 +0200

    Manage server SSH user

--- linux/server/setup.sh
+++ linux/server/setup.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 set -eu
 
+server_user=mattis
+server_user_home="/home/$server_user"
+server_user_shell=/usr/bin/zsh
+server_user_authorized_keys="$PWD/linux/server/users/$server_user/authorized_keys"
+
+if ! getent passwd "$server_user" >/dev/null; then
+  sudo useradd -m -s "$server_user_shell" "$server_user"
+fi
+
+sudo usermod -s "$server_user_shell" "$server_user"
+sudo passwd -l "$server_user" >/dev/null
+sudo install -d -m 700 -o "$server_user" -g "$server_user" "$server_user_home/.ssh"
+sudo install -m 600 -o "$server_user" -g "$server_user" \
+  "$server_user_authorized_keys" "$server_user_home/.ssh/authorized_keys"
+
 sudo install -d -m 755 /etc/systemd/journald.conf.d
 printf '%s\n' '[Journal]' 'SystemMaxUse=1G' 'RuntimeMaxUse=512M' |
   sudo tee /etc/systemd/journald.conf.d/10-size-limits.conf >/dev/null
